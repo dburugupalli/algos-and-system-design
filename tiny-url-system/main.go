c3cd578ce433c3f28f91af4e36a822048fcfb9b1
Dinakara Sai Santosh Burugupalli <53957775+dburugupalli@users.noreply.github.com>
2022-09-02 22:49:35 -0700

    Update main.go

--- tiny-url-system/main.go
+++ tiny-url-system/main.go
@@ -1,1 +1,19 @@
 
+
+
+package main
+import (
+	"log"		
+)
+
+func init(){
+	log.SetPrefix("LOG: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+	log.Println("init started")
+}
+
+func main() {
+// Println writes to the standard logger.
+	log.Println(" TODO ")
+
+}

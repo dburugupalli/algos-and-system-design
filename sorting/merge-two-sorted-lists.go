b219cf61012d64472c2406978030b6f6552fcc9a
Dinakara Sai Santosh Burugupalli <53957775+dburugupalli@users.noreply.github.com>
2022-09-05 20:54:06 -0700

    Create merge-two-sorted-lists.go

---
+++ sorting/merge-two-sorted-lists.go
@@ -0,0 +1,32 @@
+main pkg
+
+import "fmt"
+
+
+
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	result := &ListNode{}
+	tail := result
+
+	for (list1 != nil) && (list2 != nil) {
+		if list1.Val < list2.Val {
+			tail.Next = list1
+			list1 = list1.Next
+		} else {
+			tail.Next = list2
+			list2 = list2.Next
+		}
+		tail = tail.Next
+	}
+	if list1 == nil {
+		tail.Next = list2
+	} else {
+		tail.Next = list1
+	}
+	return result.Next
+}
+
+func main(){
+  mergeTwoLists()
+}
